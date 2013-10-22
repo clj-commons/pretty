2727776daa5f5248988ba5c7846ffb1bcf8d567b
Howard M. Lewis Ship <hlship@gmail.com>
2013-10-22 09:51:06 -0700

    Add support for displaying byte arrays and differences between two byte arrays

---
+++ src/io/aviso/binary.clj
@@ -0,0 +1,94 @@
+(ns io.aviso.binary
+  "Utilities for formatting binary data (byte arrays) or binary deltas."
+  (require [io.aviso.ansi :as ansi]))
+
+(def ^:private ^:const bytes-per-diff-line 16)
+(def ^:private ^:const bytes-per-line (* 2 bytes-per-diff-line))
+
+(defn- format-line
+  [offset ^bytes data line-count]
+  (format
+    "%04X:%s%n"
+    offset
+    (apply str
+           (for [i (range line-count)]
+             (format " %02X" (aget data (+ offset i)))))))
+
+(defn format-byte-array
+  "Formats an array of bytes into a hex-dump string, consisting of multiple lines; each line formatted as:
+
+  0000: 4E 6F 77 20 69 73 20 74 68 65 20 74 69 6D 65 20 66 6F 72 20 61 6C 6C 20 67 6F 6F 64 20 6D 65 6E
+  0020: 20 74 6F 20 63 6F 6D 65 20 74 6F 20 74 68 65 20 61 69 64 20 6F 66 20 74 68 65 69 72 20 63 6F 75
+  0040: 6E 74 72 79
+
+  (32 bytes per line)
+
+  ... that is, a four-byte offset, then up-to 32 bytes (depending on the length of the array)."
+  [data]
+  (loop [offset 0
+         blocks []]
+    (let [remaining (- (alength data) offset)]
+      (if (< remaining 1)
+        (apply str blocks)
+        (recur (+ bytes-per-line offset)
+               (conj blocks
+                     (format-line offset data (min bytes-per-line remaining))))))))
+
+
+(defn- match?
+  [byte-offset data-length data alternate-length alternate]
+  (and
+    (< byte-offset data-length)
+    (< byte-offset alternate-length)
+    (== (aget data byte-offset) (aget alternate byte-offset))))
+
+(defn- to-hex
+  [byte-array byte-offset]
+  (format "%02X" (aget byte-array byte-offset)))
+
+(defn- format-byte-deltas
+  [ansi-color pad? offset data-length data alternate-length alternate]
+  (apply str
+         (for [i (range bytes-per-diff-line)]
+           (let [byte-offset (+ offset i)]
+             (cond
+               ;; Exact match on both sides is easy, just print it out.
+               (match? byte-offset data-length data alternate-length alternate) (str " " (to-hex data byte-offset))
+               ;; Some kind of mismatch, so decorate with this side's color
+               (< byte-offset data-length) (str " " (ansi-color (to-hex data byte-offset)))
+               ;; Are we out of data on this side?  Print a "--" decorated with the color.
+               (< byte-offset alternate-length) (str " " (ansi-color "--"))
+               ;; This side must be longer than the alternate side.
+               ;; On the left/green side, we need to pad with spaces
+               pad? "   "
+               ;; On the right/red side, we need nothing.
+               :else "")))))
+
+(defn- format-delta-line
+  [offset expected-length ^bytes expected actual-length actual]
+  (let [line-count (max (min bytes-per-diff-line (- expected-length offset))
+                        (min bytes-per-diff-line (- actual-length offset)))]
+    (format "%04X:%s |%s%n"
+            offset
+            (format-byte-deltas ansi/bold-green true offset expected-length expected actual-length actual)
+            (format-byte-deltas ansi/bold-red false offset actual-length actual expected-length expected))))
+
+(defn format-byte-array-delta
+  "Formats a hex dump of the expected bytes (on the left) and actual bytes (on the right). Bytes
+  that do not match are highlighted in green on the expected side, and red on the actual side.
+  If the expected bytes are shorted than the actual, then it will be padded with -- placeholders (highlighted
+  in red). Likewise, if the expected bytes length exceeds the actual, then the expected side will also
+  be padded with -- placeholders (in green).
+
+  Display 16 bytes (from each data set) per line."
+  [expected actual]
+  (let [expected-length (alength expected)
+        actual-length (alength actual)
+        target-length (max actual-length expected-length)]
+    (loop [offset 0
+           blocks []]
+      (if (< (- target-length offset) 1)
+        (apply str blocks)
+        (recur (+ bytes-per-diff-line offset)
+               (conj blocks
+                     (format-delta-line offset expected-length expected actual-length actual)))))))
