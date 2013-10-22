845cbca83e2c0b98f78b609aab54f0f296ccf012
Howard M. Lewis Ship <hlship@gmail.com>
2013-10-22 12:02:23 -0700

    Define a protocol for byte-oriented access Extend protocol onto byte[], String, and nil

--- src/io/aviso/binary.clj
+++ src/io/aviso/binary.clj
@@ -2,20 +2,45 @@
   "Utilities for formatting binary data (byte arrays) or binary deltas."
   (require [io.aviso.ansi :as ansi]))
 
+(defprotocol ByteData
+  "Allows various data sources to be treated as a byte-array data type that
+  supports a length and random access to individual bytes."
+
+  (data-length [this] "The total number of bytes available.")
+  (byte-at [this index] "The byte value at a specific offset."))
+
+(extend-type (Class/forName "[B")
+  ByteData
+  (data-length [ary] (alength ary))
+  (byte-at [ary index] (aget ary index)))
+
+;;; Extends String as a convenience; assumes that the
+;;; String is in utf-8.
+
+(extend-type String
+  ByteData
+  (data-length [s] (.length s))
+  (byte-at [s index] (-> s (.charAt index) byte)))
+
+(extend-type nil
+  ByteData
+  (data-length [this] 0)
+  (byte-at [this index] (throw (IndexOutOfBoundsException. "Can't use byte-at with nil."))))
+
 (def ^:private ^:const bytes-per-diff-line 16)
 (def ^:private ^:const bytes-per-line (* 2 bytes-per-diff-line))
 
 (defn- format-line
-  [offset ^bytes data line-count]
+  [offset data line-count]
   (format
     "%04X:%s%n"
     offset
     (apply str
            (for [i (range line-count)]
-             (format " %02X" (aget data (+ offset i)))))))
+             (format " %02X" (byte-at data (+ offset i)))))))
 
-(defn format-byte-array
-  "Formats an array of bytes into a hex-dump string, consisting of multiple lines; each line formatted as:
+(defn format-byte-data
+  "Formats a ByteData into a hex-dump string, consisting of multiple lines; each line formatted as:
 
   0000: 4E 6F 77 20 69 73 20 74 68 65 20 74 69 6D 65 20 66 6F 72 20 61 6C 6C 20 67 6F 6F 64 20 6D 65 6E
   0020: 20 74 6F 20 63 6F 6D 65 20 74 6F 20 74 68 65 20 61 69 64 20 6F 66 20 74 68 65 69 72 20 63 6F 75
@@ -27,7 +52,7 @@
   [data]
   (loop [offset 0
          blocks []]
-    (let [remaining (- (alength data) offset)]
+    (let [remaining (- (data-length data) offset)]
       (if (< remaining 1)
         (apply str blocks)
         (recur (+ bytes-per-line offset)
@@ -40,11 +65,11 @@
   (and
     (< byte-offset data-length)
     (< byte-offset alternate-length)
-    (== (aget data byte-offset) (aget alternate byte-offset))))
+    (== (byte-at data byte-offset) (byte-at alternate byte-offset))))
 
 (defn- to-hex
   [byte-array byte-offset]
-  (format "%02X" (aget byte-array byte-offset)))
+  (format "%02X" (byte-at byte-array byte-offset)))
 
 (defn- format-byte-deltas
   [ansi-color pad? offset data-length data alternate-length alternate]
@@ -73,17 +98,20 @@
             (format-byte-deltas ansi/bold-green true offset expected-length expected actual-length actual)
             (format-byte-deltas ansi/bold-red false offset actual-length actual expected-length expected))))
 
-(defn format-byte-array-delta
+(defn format-byte-delta
   "Formats a hex dump of the expected bytes (on the left) and actual bytes (on the right). Bytes
   that do not match are highlighted in green on the expected side, and red on the actual side.
   If the expected bytes are shorted than the actual, then it will be padded with -- placeholders (highlighted
   in red). Likewise, if the expected bytes length exceeds the actual, then the expected side will also
   be padded with -- placeholders (in green).
 
+  expected - ByteData
+  actual - ByteData
+
   Display 16 bytes (from each data set) per line."
   [expected actual]
-  (let [expected-length (alength expected)
-        actual-length (alength actual)
+  (let [expected-length (data-length expected)
+        actual-length (data-length actual)
         target-length (max actual-length expected-length)]
     (loop [offset 0
            blocks []]
