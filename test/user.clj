e10d266f3d024013780a66f32bad45dd876b3cd2
Howard M. Lewis Ship <hlship@gmail.com>
2013-10-28 09:38:21 -0700

    Add a user namespace to assist with testing

---
+++ test/user.clj
@@ -0,0 +1,10 @@
+(ns user
+  (import (java.sql SQLException))
+  (use [io.aviso ansi binary exception]))
+
+(defn make-exception
+  "Creates a sample exception used to test the exception formatting logic."
+  []
+  (->> (SQLException. "Database failure" "ABC" 123)
+       (RuntimeException. "Failure updating row")
+       (RuntimeException. "Request handling exception")))
