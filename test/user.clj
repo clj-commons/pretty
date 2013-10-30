87d2a77d50cbcd389800bce950ba0c0976eea098
Howard M. Lewis Ship <hlship@gmail.com>
2013-10-29 20:57:24 -0700

    Break the construction of the exception into slightly more realistic steps

--- test/user.clj
+++ test/user.clj
@@ -3,9 +3,22 @@
   (use [io.aviso ansi binary exception]
        [clojure test pprint]))
 
+(defn- jdbc-update
+  []
+  (throw (SQLException. "Database failure" "ABC" 123)))
+
+(defn- update-row
+  []
+  (try
+    (jdbc-update)
+    (catch Throwable e
+      (throw (RuntimeException. "Failure updating row" e)))))
+
 (defn make-exception
   "Creates a sample exception used to test the exception formatting logic."
   []
-  (->> (SQLException. "Database failure" "ABC" 123)
-       (RuntimeException. "Failure updating row")
-       (RuntimeException. "Request handling exception")))
+  (try
+    (update-row)
+    (catch Throwable e
+      ;; Return it, not rethrow it.
+      (RuntimeException. "Request handling exception" e))))
