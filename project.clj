68b4707b83bf09c758ca58c23b747a643b0e42bb
Howard M. Lewis Ship <hlship@gmail.com>
2013-11-05 14:38:25 -0800

    Configure Codox

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject io.aviso/pretty "0.1.2"
   :description "Clojure library to help print things, prettily"
   :url "https://github.com/AvisoNovate/pretty"
-  :license {:name "Apache Sofware Licencse 2.0"
-            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+  :license {:name "Apache Sofware License 2.0"
+            :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
   ;; Normally we don't AOT compile; only when tracking down reflection warnings.
-  :profiles {:reflection-warnings {:aot :all
+  :profiles {:reflection-warnings {:aot         :all
                                    :global-vars {*warn-on-reflection* true}}}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :codox {:src-dir-uri               "https://github.com/AvisoNovate/pretty/blob/master/"
+          :src-linenum-anchor-prefix "L"}
+  )
