7b0317657a9ac7b7c4b12f62c99909b054dcf0fa
Howard M. Lewis Ship <hlship@gmail.com>
2014-05-12 08:28:45 -0700

    Advance version number

--- project.clj
+++ project.clj
@@ -1,12 +1,10 @@
-(defproject io.aviso/pretty "0.1.10"
+(defproject io.aviso/pretty "0.1.11-SNAPSHOT"
   :description "Clojure library to help print things, prettily"
   :url "https://github.com/AvisoNovate/pretty"
   :license {:name "Apache Sofware License 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
   ;; Normally we don't AOT compile; only when tracking down reflection warnings.
-  :profiles {:reflection-warnings {:aot         :all
-                                   :global-vars {*warn-on-reflection* true}}}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :codox {:src-dir-uri               "https://github.com/AvisoNovate/pretty/blob/master/"
           :src-linenum-anchor-prefix "L"}
   )
