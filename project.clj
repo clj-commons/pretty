ccde00ef0bfc848ac3c7c2041c8c3a88d68b6daa
Howard M. Lewis Ship <hlship@gmail.com>
2014-05-14 14:01:37 -0700

    Advance version number for release 0.1.12

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.aviso/pretty "0.1.11"
+(defproject io.aviso/pretty "0.1.12-SNAPSHOT"
   :description "Clojure library to help print things, prettily"
   :url "https://github.com/AvisoNovate/pretty"
   :license {:name "Apache Sofware License 2.0"
