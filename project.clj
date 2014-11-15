8a60a11b617915acc6c94f70af608befa136a28b
Howard M. Lewis Ship <hlship@gmail.com>
2014-11-14 16:46:08 -0800

    Prepare for 0.1.13 release

--- project.clj
+++ project.clj
@@ -1,10 +1,19 @@
 (defproject io.aviso/pretty "0.1.13"
-  :description "Clojure library to help print things, prettily"
-  :url "https://github.com/AvisoNovate/pretty"
-  :license {:name "Apache Sofware License 2.0"
-            :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  ;; Normally we don't AOT compile; only when tracking down reflection warnings.
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :codox {:src-dir-uri               "https://github.com/AvisoNovate/pretty/blob/master/"
-          :src-linenum-anchor-prefix "L"
-          :defaults                  {:doc/format :markdown}})
+            :description "Clojure library to help print things, prettily"
+            :url "https://github.com/AvisoNovate/pretty"
+            :license {:name "Apache Sofware License 2.0"
+                      :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
+            ;; Normally we don't AOT compile; only when tracking down reflection warnings.
+            :dependencies [[org.clojure/clojure "1.6.0"]]
+            :plugins [[lein-shell "0.4.0"]]
+            :shell {:commands {"scp" {:dir "doc"}}}
+            :aliases {"deploy-doc" ["shell"
+                                    "scp" "-r" "." "hlship_howardlewisship@ssh.phx.nearlyfreespeech.net:io.aviso/pretty"]
+                      "release"    ["do"
+                                    "clean,"
+                                    "doc,"
+                                    "deploy-doc,"
+                                    "deploy" "clojars"]}
+            :codox {:src-dir-uri               "https://github.com/AvisoNovate/pretty/blob/master/"
+                    :src-linenum-anchor-prefix "L"
+                    :defaults                  {:doc/format :markdown}})
