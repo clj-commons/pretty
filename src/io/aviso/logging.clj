3d44fdaf2f0e61d0cbba9874871c406390cf95a2
Howard M. Lewis Ship <hlship@gmail.com>
2015-02-04 11:20:23 -0800

    Modify logging to not override the filter, allowing the default to come from *default-frame-filter*

--- src/io/aviso/logging.clj
+++ src/io/aviso/logging.clj
@@ -4,7 +4,6 @@
   You must [add clojure.tools.logging as an explicit dependency](https://github.com/clojure/tools.logging) of your project."
   {:added "0.1.15"}
   (:require [clojure.tools.logging :as l]
-            [io.aviso.repl :as repl]
             [io.aviso.exception :as e]
             [io.aviso.writer :as writer])
   (:import [java.lang.Thread$UncaughtExceptionHandler]))
@@ -12,7 +11,18 @@
 (defn install-pretty-logging
   "Modifies clojure.tools.logging to use pretty exception logging."
   ([]
-    (install-pretty-logging repl/standard-frame-filter))
+    (alter-var-root
+      #'l/log*
+      (fn [default-impl]
+        (fn [logger level throwable message]
+          (default-impl logger
+                        level
+                        nil
+                        (if throwable
+                          (str message
+                               writer/eol
+                               (e/format-exception throwable))
+                          message))))))
   ([frame-filter-fn]
     (alter-var-root
       #'l/log*
