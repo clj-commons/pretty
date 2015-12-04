36c6c10a141a2b810bd8c9c001a2b3b4f0570e9a
Howard M. Lewis Ship <hlship@walmartlabs.com>
2015-12-04 11:24:42 -0800

    Use Clojure 1.6 as the baseline for Pretty

--- project.clj
+++ project.clj
@@ -3,11 +3,12 @@
   :url "https://github.com/AvisoNovate/pretty"
   :license {:name "Apache Sofware License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.1" :optional true]]
   :plugins [[lein-shell "0.4.0"]
             [lein-codox "0.9.0"]]
   :profiles
+  {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]} }
   {:1.8 {:dependencies [[org.clojure/clojure "1.8.0-RC3"]]} }
   :shell {:commands {"scp" {:dir "target/doc"}}}
   :aliases {"deploy-doc" ["shell"
