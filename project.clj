5834bd6e0809468f8977439865bc56e4e8bb9cda
Howard M. Lewis Ship <hlship@walmartlabs.com>
2016-01-07 11:46:20 -0800

    Fix incorrect definition of 1.7 and 1.8 profiles

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/tools.logging "0.3.1" :optional true]]
   :plugins [[lein-codox "0.9.0"]]
-  :profiles {{:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
-             {:1.8 {:dependencies [[org.clojure/clojure "1.8.0-RC3"]]}}}
+  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-RC3"]]}}
   ;; Part of release is currently manual; copy target/docs to the AvisoNovate/docs/pretty folder
   :aliases {"release" ["do"
                        "clean,"
