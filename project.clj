a5c7b097a03a1b6b71cda34a806e55bd23c18dc5
Howard M. Lewis Ship <hlship@walmartlabs.com>
2016-02-11 12:10:33 -0800

    Latest version of codox

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/tools.logging "0.3.1" :optional true]]
-  :plugins [[lein-codox "0.9.0"]]
+  :plugins [[lein-codox "0.9.3"]]
   :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :dev {:dependencies [[criterium "0.4.3"]]}}
