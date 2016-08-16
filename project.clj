c8f735f67b90b7b9a0038d4287dcafea4951d6c8
Paudi Moriarty <paudi.moriarty@aviso.io>
2016-08-16 16:57:41 +0100

    Set version to 0.1.31-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.aviso/pretty "0.1.30"
+(defproject io.aviso/pretty "0.1.31-SNAPSHOT"
   :description "Clojure library to help print things, prettily"
   :url "https://github.com/AvisoNovate/pretty"
   :license {:name "Apache Sofware License 2.0"
