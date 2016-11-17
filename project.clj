0771c8250f60427cb96d82c7203b4b6eaecf9593
Howard M. Lewis Ship <hlship@walmartlabs.com>
2016-11-16 17:10:59 -0800

    Add a docs alias to run codox with the correct classpath

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/tools.logging "0.3.1" :optional true]]
-  :plugins [[lein-codox "0.9.3"]]
+  :plugins [[lein-codox "0.10.1"]]
   :profiles {:1.7  {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :1.9  {:dependencies [[org.clojure/clojure "1.9.0-alpha10"]]}
@@ -14,6 +14,7 @@
   ;; Part of release is currently manual; copy target/docs to the AvisoNovate/docs/pretty folder
   :aliases {"release" ["do"
                        "clean,"
-                       "deploy" "clojars"]}
+                       "deploy" "clojars"]
+            "docs" ["with-profiles" "+lein,+1.8" "codox"]}
   :codox {:source-uri "https://github.com/AvisoNovate/pretty/blob/master/{filepath}#L{line}"
           :metadata   {:doc/format :markdown}})
