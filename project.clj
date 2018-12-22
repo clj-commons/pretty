56aa6539205ee2aaf9e5fb79129a56cf1915a907
Paudi Moriarty <90610+pmoriarty@users.noreply.github.com>
2018-12-22 15:35:53 +0000

    Prepare for 0.1.36

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.aviso/pretty "0.1.35"
+(defproject io.aviso/pretty "0.1.36"
   :description "Clojure library to help print things, prettily"
   :url "https://github.com/AvisoNovate/pretty"
   :license {:name "Apache Sofware License 2.0"
